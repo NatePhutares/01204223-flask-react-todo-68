625acd494cc0ad9d0e1ca31f8ddb35a23535f821
Natrius <natephutare@gmail.com>
2026-02-21 12:35:13 +0700

    create login api and add jwt_required to other route

--- backend/main.py
+++ backend/main.py
@@ -3,16 +3,23 @@ from flask_cors import CORS
 from flask_migrate import Migrate 
 import click
 from models import User, TodoItem, Comment, db 
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
 
+# database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
-
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# JWT
+app.config['JWT_SECRET_KEY'] = 'fdsjkfjioi2rjshr2345hrsh043j5oij5545'
+jwt = JWTManager(app)
+
 @app.route('/api/todos/', methods=['GET'])
+@jwt_required()
 def get_todos():
     todos = TodoItem.query.all()
     return jsonify([todo.to_dict() for todo in todos])
@@ -23,6 +30,7 @@ def new_todo(data):
 
 
 @app.route('/api/todos/', methods=['POST'])
+@jwt_required()
 def add_todo():
     data = request.get_json()
     todo = new_todo(data)
@@ -36,6 +44,7 @@ def add_todo():
     
 
 @app.route('/api/todos/<int:id>/toggle/', methods=['PATCH'])
+@jwt_required()
 def toggle_todo(id):
     todo = TodoItem.query.get_or_404(id)
     todo.done = not todo.done
@@ -44,6 +53,7 @@ def toggle_todo(id):
 
 
 @app.route('/api/todos/<int:id>/', methods=['DELETE'])
+@jwt_required()
 def delete_todo(id):
     todo = TodoItem.query.get_or_404(id)
     db.session.delete(todo)
@@ -52,6 +62,7 @@ def delete_todo(id):
 
 
 @app.route('/api/todos/<int:todo_id>/comments/', methods=['POST'])
+@jwt_required()
 def add_comment(todo_id):
     todo_item = TodoItem.query.get_or_404(todo_id)
 
@@ -67,6 +78,21 @@ def add_comment(todo_id):
     db.session.commit()
  
     return jsonify(comment.to_dict())
+
+
+@app.route('/api/login/', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({'error': 'Username and password are required'}), 400
+
+    user = User.query.filter_by(username=data['username']).first()
+    if not user or not user.check_password(data['password']):
+        return jsonify({'error': 'Invalid username or password'}), 401
+
+    access_token = create_access_token(identity=user.username)
+    return jsonify(access_token=access_token)
+
 
 @app.cli.command("create-user")
 @click.argument("username")
